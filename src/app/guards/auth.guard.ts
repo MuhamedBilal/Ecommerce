c25deae29f7de337028ef6e066b954ebfed14bd0
MuhamedBilal <mohamedbelal109@gmail.com>
2023-06-14 07:44:30 +0300

    authguard edit

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,7 @@
-// import { CanActivateFn } from '@angular/router';
-// import { Injectable } from '@angular/core';
+import { CanActivateFn} from '@angular/router';
 
-
-// export const authGuard: CanActivateFn = (route, state) => {
-//   return true;
-// };
-
-import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+export const authGuard: CanActivateFn = (route, state) => {
   // Check if user is logged in
   const isLoggedIn = localStorage.getItem('username') !== null;
   return isLoggedIn;
 };
-
